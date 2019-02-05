bbb5eb5ce4bdeba83a13a2f2bd3a3d40c1fb3a59
Dayton Wilks <dayton.wilks@labs.cset.oit.edu>
2019-02-04 18:11:07 -0800

    Added Result List Code & HTML

--- assets/js/NewView.js
+++ assets/js/NewView.js
@@ -1,4 +1,6 @@
 const { dialog } = require('electron').remote;
+const { shell } = require('electron');
+const path = require('path');
 var ffmpeg_static = require('ffmpeg-static');
 var fluent_ffmpeg = require('fluent-ffmpeg');
 fluent_ffmpeg.setFfmpegPath(ffmpeg_static.path);
@@ -6,6 +8,7 @@ fluent_ffmpeg.setFfmpegPath(ffmpeg_static.path);
 const supportedImageFormats = ["jpeg", "png", "bmp", "tiff", "gif"];
 const supportedVideoFormats = ["avi", "mp4"];
 const supportedAudioFormats = [];
+const DEBUG = 1;
 
 if(typeof($.fn.popover) != 'undefined') {
     console.log("Bootstrap")
@@ -13,24 +16,45 @@ if(typeof($.fn.popover) != 'undefined') {
 
 function GetLocalFile(elementName) {
     let filePath = dialog.showOpenDialog({properties: ["openFile"]});
+    //filePath = new String(filePath);
     let element = document.getElementById(elementName);
 
     element.title = filePath[0];
+    //element.value = path.basename(filePath);
     element.value = filePath[0].slice(filePath[0].lastIndexOf("\\") + 1);
 }
 
 function SaveLocalFile(elementName) {
     let filePath = dialog.showSaveDialog();
+    //filePath = new String(filePath);
     let element = document.getElementById(elementName);
 
     element.title = filePath;
     element.value = filePath.slice(filePath.lastIndexOf("\\") + 1);
+    //element.value = path.basename(filePath);
 }
 
+function msToMinSec(time) {
+    let result = {Minutes: 0, Seconds: 0};
+
+    time /= 1000;
+    result.Minutes = Math.floor(time / 60);
+    result.Seconds = Math.floor(time % 60);
+    
+    return result;
+}
+
+
 function ConvertVideo(source, destination, format) {
+    let startTime;
     let proc = fluent_ffmpeg(source)
         .toFormat(format)
-        .on('end', () => { console.log('file has been converted succesfully'); })
+        .on('start', () => { 
+            startTime = new Date(); console.log({startTime: startTime});
+        })
+        .on('end', (stdout, stderr) => { 
+            AppendVideoResultElement(source, destination, msToMinSec(new Date() - startTime));
+        })
         .on('error', (err) => { console.log('an error happened: ' + err.message); })
         .save(destination);
 }
@@ -46,4 +70,55 @@ function ConvertVideoButton() {
 
     if (input.slice(input.lastIndexOf('.')) != operation)
         ConvertVideo(input, output, operation);
+}
+
+function AVRE_HeaderHelper(duration) {
+    let subElement = document.createElement('div');
+    subElement.setAttribute('class', 'card-header');
+    subElement.innerText = 'Task - ' + duration.Minutes + ':' + duration.Seconds;
+    return subElement;
+}
+
+function AVRE_ButtonHelper(pathString) {
+    let button = document.createElement('button');
+        button.setAttribute('type', 'button');
+        button.setAttribute('class', 'btn btn-primary');
+        button.setAttribute('onclick', "shell.showItemInFolder(" + JSON.stringify(pathString) + ")");
+        button.innerText = 'Open';
+    return button;
+}
+
+function AVRE_LabelHelper(pathString) {
+    let label = document.createElement('label');
+    label.innerText = path.basename(pathString);
+    return label;
+}
+
+function AVRE_CardHelper(pathString) {
+    let subElement = document.createElement('div');
+    subElement.setAttribute('class', 'card-body');
+    { 
+        subElement.appendChild(AVRE_ButtonHelper(pathString));
+        subElement.appendChild(AVRE_LabelHelper(pathString));
+    }
+    return subElement;
+}
+
+function AppendVideoResultElement(source, destination, duration) {
+    // Create Element to Append
+    let element = document.createElement('div');
+    element.setAttribute('class', 'card border-secondary');
+
+    // Create Task Header
+    element.appendChild(AVRE_HeaderHelper(duration));
+
+    // Create Source Section
+    element.appendChild(AVRE_CardHelper(source));
+
+    // Create Destination Section
+    element.appendChild(AVRE_CardHelper(destination));
+    
+    // Add Element to Document
+    let resultContainer = document.getElementById('resultContainer');
+    resultContainer.appendChild(element);
 }
