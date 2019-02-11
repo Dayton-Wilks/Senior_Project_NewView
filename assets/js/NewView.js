763a5ca8ef7ad874fac0b736cfccb959779f7da5
dayton.wilks <d.wilks.trash@gmail.com>
2019-02-11 00:27:49 -0800

    OAUTH2 Working - Can list drive files to console

--- assets/js/NewView.js
+++ assets/js/NewView.js
@@ -318,112 +318,21 @@ function ConvertVideoButton() {
 // const opn = require('opn');
 // const destroyer = require('server-destroy');
 
-// // Download your OAuth2 configuration from the Google
-// //const keys = require('./oauth2.keys.json');
-
-// /**
-//  * Start by acquiring a pre-authenticated oAuth2 client.
-//  */
-// async function main() {
-//   const oAuth2Client = await getAuthenticatedClient();
-//   // Make a simple request to the People API using our pre-authenticated client. The `request()` method
-//   // takes an GaxiosOptions object.  Visit https://github.com/JustinBeckwith/gaxios.
-// //   const url = 'https://people.googleapis.com/v1/people/me?personFields=names';
-// //   const res = await oAuth2Client.request({url});
-// const drive = google.drive({version: 'v3', oauth2Client});
-//     drive.files.list({
-//         pageSize: 10,
-//         fields: 'nextPageToken, files(id, name)',
-//         }, 
-//         (err, res) => {
-//             if (err) return console.log('The API returned an error: ' + err);
-//             const files = res.data.files;
-//             if (files.length) {
-//             console.log('Files:');
-//             files.map((file) => {
-//                 console.log(`${file.name} (${file.id})`);
-//             });
-//             } else {
-//             console.log('No files found.');
-//         }
-//     });
-//   //console.log(res.data);
-
-//   // After acquiring an access_token, you may want to check on the audience, expiration,
-//   // or original scopes requested.  You can do that with the `getTokenInfo` method.
-//   const tokenInfo = await oAuth2Client.getTokenInfo(
-//     oAuth2Client.credentials.access_token
-//   );
-//   console.log(tokenInfo);
-// }
-
-// /**
-//  * Create a new OAuth2Client, and go through the OAuth2 content
-//  * workflow.  Return the full client to the callback.
-//  */
-// function getAuthenticatedClient() {
-//   return new Promise((resolve, reject) => {
-//     // create an oAuth client to authorize the API call.  Secrets are kept in a `keys.json` file,
-//     // which should be downloaded from the Google Developers Console.
-//     const oAuth2Client = new OAuth2Client(
-
-//         "http://localhost:3000"
-//     );
-
-//     // Generate the url that will be used for the consent dialog.
-//     const authorizeUrl = oAuth2Client.generateAuthUrl({
-//       access_type: 'offline',
-//       scope: 'https://www.googleapis.com/auth/drive.metadata.readonly',
-//     });
-
-//     // Open an http server to accept the oauth callback. In this simple example, the
-//     // only request to our webserver is to /oauth2callback?code=<code>
-//     const server = http
-//       .createServer(async (req, res) => {
-//           console.log('Creating server')
-//         try {
-//           if (req.url.indexOf('/oauth2callback') > -1) {
-//             // acquire the code from the querystring, and close the web server.
-//             const qs = new url.URL(req.url, 'http://localhost:3000')
-//               .searchParams;
-//             const code = qs.get('code');
-//             console.log(`Code is ${code}`);
-//             res.end('Authentication successful! Please return to the console.');
-//             server.destroy();
-
-//             // Now that we have the code, use that to acquire tokens.
-//             const r = await oAuth2Client.getToken(code);
-//             // Make sure to set the credentials on the OAuth2 client.
-//             oAuth2Client.setCredentials(r.tokens);
-//             console.info('Tokens acquired.');
-//             resolve(oAuth2Client);
-//           }
-//         } catch (e) {
-//           reject(e);
-//         }
-//       })
-//       .listen(3000, () => {
-//         console.log('Listening to server')
-//         // open the browser to the authorize url to start the workflow
-//         opn(authorizeUrl, {wait: false}).then(cp => cp.unref());
-//       });
-//       console.log('Destroying server')
-//     destroyer(server);
-//   });
-// }
-
-// main().catch(console.error);
-
-
 const {google} = require('googleapis');
 const { BrowserWindow } = require('electron').remote; 
 const  app = require('electron').remote.app;
 
 var basepath = app.getAppPath();
 
+const sec = {
+    client_id:"<INSERT>",
+    client_secret:"<INSERT>",
+    auth_uri:"https://oauth2.googleapis.com/token"
+}
+
 const oauth2Client = new google.auth.OAuth2(
-    "clientid",
-    "secret?",
+    sec.client_id,
+    sec.client_secret,
     "http://localhost"
   );
 
@@ -432,10 +341,7 @@ const scopes = [
 ];
 
 const url = oauth2Client.generateAuthUrl({
-    // 'online' (default) or 'offline' (gets refresh_token)
     access_type: 'offline',
-    approval_prompt : 'force',
-    // If you only need one scope you can pass it as a string
     scope: scopes
 });
  
@@ -446,7 +352,7 @@ var authWindow = new BrowserWindow(
     show: false, 
     'node-integration': true 
 });
-console.log(url);
+
 authWindow.loadURL(url);
 authWindow.show();
 
@@ -456,46 +362,48 @@ function handleCallback (url) {
     var error = /\?error=(.+)$/.exec(url);
   
     if (code || error) {
-      // Close the browser if code found or error
       authWindow.destroy();
     }
-  
-    // If there is a code, proceed to get token from github
+
     if (code) {
-        console.log('good');
-        // let tokens = oauth2Client.getToken(code, (err, t) => {if (err) console.log('Error getting tokens')});
-        // console.log({url, code});
-        // oauth2Client.setCredentials(tokens);
-
-
-
         const request = require('request');
+        const token_uri = sec.auth_uri;
 
-        request.get(url, { json: true }, (err, res, body) => {
-        if (err) { console.log(err); }
-        console.log(url);
-        console.log(res);
-        console.log(body);
-        });
-          
+        request.post(token_uri, { form:
+            {client_id: sec.client_id,
+            client_secret: sec.client_secret,
+            code: code,
+            grant_type: "authorization_code",
+            redirect_uri: 'http://localhost'}
+            },
+            (err, httpResponse, body) => {
+                console.log({err, httpResponse, body});
+                const cred = JSON.parse(body);
+                console.log(cred);
 
-        const drive = google.drive({version: 'v3', oauth2Client});
-        drive.files.list({
-            pageSize: 10,
-            fields: 'nextPageToken, files(id, name)',
-            }, 
-            (err, res) => {
-                if (err) return console.log('The API returned an error: ' + err);
-                const files = res.data.files;
-                if (files.length) {
-                console.log('Files:');
-                files.map((file) => {
-                    console.log(`${file.name} (${file.id})`);
+                oauth2Client.setCredentials(cred);
+
+
+                const drive = google.drive({version: 'v3', auth: oauth2Client});
+                drive.files.list({
+                    pageSize: 10,
+                    fields: 'nextPageToken, files(id, name)',
+                    }, 
+                    (err, res) => {
+                        if (err) return console.log('The API returned an error: ' + err);
+                        const files = res.data.files;
+                        if (files.length) {
+                        console.log('Files:');
+                        files.map((file) => {
+                            console.log(`${file.name} (${file.id})`);
+                        });
+                        } else {
+                        console.log('No files found.');
+                    }
                 });
-                } else {
-                console.log('No files found.');
             }
-        });
+        );
+          
     } else if (error) {
       alert('Oops! Something went wrong and we couldn\'t' +
         'log you in using Github. Please try again.');
@@ -518,25 +426,3 @@ authWindow.on(
     () => {authWindow = null;},
     false
 );
-
-// Each API may support multiple version. With this sample, we're getting
-// v3 of the blogger API, and using an API key to authenticate.
-// const GD = google.drive({
-//   version: 'v3',
-//   auth: oauth2Client
-// });
-
-
- 
-// const params = {
-//   blogId: 3213900
-// };
- 
-// // get the blog details
-// blogger.blogs.get(params, (err, res) => {
-//   if (err) {
-//     console.error(err);
-//     throw err;
-//   }
-//   console.log(`The blog url is ${res.data.url}`);
-// });
